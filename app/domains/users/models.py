52e53d16ee5f2a3aa55faa088e51bbb15a638a9b
viamn <helifur17@proton.me>
2025-08-01 19:41:15 +0300

    fix: changed pk of users table

--- app/domains/users/models.py
+++ app/domains/users/models.py
@@ -6,8 +6,7 @@ from sqlmodel import Field, SQLModel
 class User(SQLModel, table=True):
     __tablename__ = "users"
 
-    id: int = Field(default=None, primary_key=True)
-    address: str = Field(default="", index=True, unique=True)
+    address: str = Field(primary_key=True, index=True, unique=True)
     chainId: str = Field(default="")
     connectedAt: datetime = Field(default_factory=datetime.now)
     lastActivity: datetime = Field(default_factory=datetime.now)
