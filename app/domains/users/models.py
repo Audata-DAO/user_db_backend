b8f3478d868f790959ce2489af2f254eec1f960a
viamn <helifur17@proton.me>
2025-08-02 22:43:21 +0300

    feat: added user_metadata model and migration

--- app/domains/users/models.py
+++ app/domains/users/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
+from typing import Optional
 
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, Relationship, SQLModel
 
 
 class User(SQLModel, table=True):
@@ -10,3 +11,19 @@ class User(SQLModel, table=True):
     chainId: str = Field(default="")
     connectedAt: datetime = Field(default_factory=datetime.now)
     lastActivity: datetime = Field(default_factory=datetime.now)
+
+    user_metadata: Optional["UserMetadata"] = Relationship(back_populates="user")
+
+
+class UserMetadata(SQLModel, table=True):
+    __tablename__ = "user_metadata"
+
+    id: int = Field(default=None, primary_key=True)
+    userAddress: str = Field(default=None, foreign_key="users.address")
+    country: str = Field(default="")
+    birthMonth: str = Field(default="")
+    birthYear: str = Field(default="")
+    isItRelated: bool = Field(default=False)
+    submittedAt: datetime = Field(default_factory=datetime.now)
+
+    user: User = Relationship(back_populates="user_metadata")
