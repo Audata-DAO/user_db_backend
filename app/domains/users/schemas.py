210c95ad3750949f11717199414af7e6ede253b4
viamn <helifur17@proton.me>
2025-08-02 22:44:19 +0300

    feat: added POST for user_metadata

--- app/domains/users/schemas.py
+++ app/domains/users/schemas.py
@@ -1,6 +1,16 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 
 
 class UserIn(BaseModel):
     address: str
     chainId: str
+
+
+class UserMetadataIn(BaseModel):
+    userAddress: str
+    country: str
+    birthMonth: str
+    birthYear: str
+    isItRelated: bool
